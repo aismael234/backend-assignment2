76f810a21ebed8873178ffae41ff19d1db26e099
aladdin <ismael.aladdin@gmail.com>
2022-09-30 10:25:20 -0700

    Add /users/:id endpoint

---
+++ backend.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
+
+const users = { 
+    users_list :
+    [
+       { 
+          id : 'xyz789',
+          name : 'Charlie',
+          job: 'Janitor',
+       },
+       {
+          id : 'abc123', 
+          name: 'Mac',
+          job: 'Bouncer',
+       },
+       {
+          id : 'ppp222', 
+          name: 'Mac',
+          job: 'Professor',
+       }, 
+       {
+          id: 'yat999', 
+          name: 'Dee',
+          job: 'Aspring actress',
+       },
+       {
+          id: 'zap555', 
+          name: 'Dennis',
+          job: 'Bartender',
+       }
+    ]
+ }
+
+ app.get('/users', (req, res) => {
+
+   const name = req.query.name;
+   if(name){
+      let result = findUserByName(name);
+      result = {users_list: result};
+      res.send(result);
+   }
+
+   else{
+    res.send(users);
+   }
+ });
+
+ const findUserByName = (name) => {
+   return users['users_list'].filter( (user) => user['name'] === name);
+ };
+
+ app.get('/users/:id', (req, res) => {
+   const id = req.params.id;
+   let result = findUserById(id);
+   if(result === undefined || result.length == 0)
+      res.status(404).send('Resource not found.');
+   else{
+      result = {users_list: result}
+      res.send(result);
+   }
+ })
+
+ const findUserById = (id) => {
+   return users['users_list'].find( (user) => user['id'] === id);
+ };
